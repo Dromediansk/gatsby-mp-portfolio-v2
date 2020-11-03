f3244797dc030ffb3fc378ed163f63d7b9d8424a
Dromediansk <miroslav.pillar@senacor.com>
2020-11-03 19:49:27 +0100

    y

--- scripts/commit.py
+++ scripts/commit.py
@@ -1,13 +1,25 @@
 import subprocess as cmd
 
-cp = cmd.run("git add -A", check=True, shell=True)
 
-response = input(
+def commitAndPush(message):
+    cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+    cmd.run("git push --set-upstream origin scripts",
+            check=True, shell=True)
+
+
+cmd.run("git add -A", check=True, shell=True)
+
+defaultResponse = input(
     "Do you want to use the default message for this commit? ('update the repository') ([y]/n)\n")
 message = "update the repository"
 
-if response.startswith('n'):
-    message = input("What message you want?\n")
+if defaultResponse.startswith('y'):
+    commitAndPush(defaultResponse)
 
-cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
-cp = cmd.run("git push --set-upstream origin scripts", check=True, shell=True)
+elif defaultResponse.startswith('n'):
+    message = input("What message you want?\n")
+    commitAndPush(message)
+else:
+    correctionAnswer = input("Please provide a correct answer ([y]/n):")
+    if correctionAnswer.startswith('n') or correctionAnswer.startswith('n'):
+        commitAndPush(correctionAnswer)
