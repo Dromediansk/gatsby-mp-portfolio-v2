eed42d37f5457c4488f8cf8cf5526d2e53357312
Dromediansk <miroslav.pillar@senacor.com>
2020-11-03 19:24:27 +0100

    testing

--- scripts/commit.py
+++ scripts/commit.py
@@ -10,6 +10,5 @@ message = "update the repository"
 if response.startswith('n'):
     message = input("What message you want?\n")
 
-cp = cmd.run("git add .", check=True, shell=True)
 cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push master -f", check=True, shell=True)
